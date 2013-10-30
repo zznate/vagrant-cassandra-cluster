d879c460a9a2cc51629eadad6006536207beb5ce
zznate <zznate.m@gmail.com>
2013-10-30 15:26:32 -0500

    working when it starts image

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,24 +1,15 @@
-#include cassandra
-#include apache2
-
-
-#exec { "apt-get update" :	
-#	command => "/usr/bin/apt-get update",
-#}
-
-#package { "apache2" :
-#  require => Exec["apt-get update"],
-#}
 
 file { "/var/www" :
   ensure => link,
   target => "/vagrant",
   force => true,
 }
-include cassandra
-
-#file { "/etc/cassandra/cassandra.yaml" :
-#	ensure => file,
-#	content => template("cassandra/templates/cassandra.yaml.erb"),
-#}
+#    file { 'cassandra config dir' :
+#    path => '/etc/cassandra',
+#    ensure => directory,
+#  }
+#  file { 'test config file':
+#    path => '/etc/cassandra/cassandrai2.yaml',
+#    content => "cass1.yaml.erb"
+#  }
 
